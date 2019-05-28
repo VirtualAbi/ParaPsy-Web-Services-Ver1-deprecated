21067fab0ff60450ab0c51949c486067cde0bc65
Abhinanth <virtualabi2.0@gmail.com>
2019-05-28 12:11:02 +0530

    Added endpoints for login and register

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+//Routes
+app.use("/", require("./routes/users"));
+
 port = process.env.PORT || 3000;
 
 app.listen(port, console.log(`Listening on port ${port}`));
